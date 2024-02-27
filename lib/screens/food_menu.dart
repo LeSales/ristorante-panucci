ff9cc4da618b94593844737eca2d7c34329258ff
leandro.sales <leandro.sales@ngi.com.br>
2024-02-27 08:41:48 -0300

    add menu page navigation

---
+++ lib/screens/food_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodMenu extends StatelessWidget {
+  const FoodMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
