47cae7d0f143b0b3d55d8675350351748bdcda9d
leandro.sales <leandro.sales@ngi.com.br>
2024-02-28 08:57:49 -0300

    create drink menu and add drink list

--- lib/components/drink_item.dart
+++ lib/components/drink_item.dart
@@ -15,6 +15,8 @@ final String itemPrice;
       child: Column(
         children: <Widget>[
           Image(
+            height: 80,
+            width: double.infinity,
             image: AssetImage(imageURI),
             fit: BoxFit.cover,
           ),
